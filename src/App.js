5183aa1b9242e33a63d2c3bc02d4ebca499e28fa
SickeningMyth7 <hridayeshpadalkar7@gmail.com>
2021-09-27 14:52:55 +0530

    Added Notecard, Tabs, Create event and data.json folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import CreateEvent from "./components/CreatEvent";
 import React from "react";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-export default function App() {
-	return (
-		<div>
-			<Signup/>
-			{/* <Login /> */}
-		</div>
-	);
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import SectionHeading from "./components/Tabs";
+import Banner from "./components/Banner";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f44336',
+    }
+  },
+});
+function App() {
+  return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <NavBar />
+            <Banner />
+            <SectionHeading />
+          </Route>
+          <Route path="/create">
+            <CreateEvent />
+          </Route>
+		  <Route path="/login">
+            <Login />
+          </Route>
+		  <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </Router>
+  );
 }
+
+export default App;
+
