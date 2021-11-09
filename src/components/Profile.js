022f25a46cbf47b4b2fa16ab7b64bd19acb3905e
Gaurav Singh <gaurav.s.140102@gmail.com>
2021-11-09 18:22:44 +0530

    Fetch current user data and populate in profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./Profile.css";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import Typography from "@material-ui/core/Typography";
@@ -6,89 +6,118 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
+import { useHistory } from "react-router";
 
-export default class Profile extends Component {
-	render() {
-		return (
-			<div>
-				<Box className="form">
-					<div className="title">
-						<AccountCircleIcon
-							style={{ fontSize: 55, marginRight: 10 }}
-							className="icon"
-						/>
-						<Typography style={{ fontSize: 40 }} className="text" variant="h2">
-							{" "}
-							Welcome Back, Username!
-						</Typography>
-					</div>
+export default function Profile() {
+	const history = useHistory();
+	const [userInfo, setUserInfo] = useState({});
+	const callProfilePage = async () => {
+		try {
+			const res = await fetch("/profile", {
+				method: "GET",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				credentials: "include",
+			});
+			const data = await res.json();
+			console.log(data);
+			setUserInfo(data);
 
-					<div className="detail">
-						<Typography
-							style={{ marginRight: 80 }}
-							className="label"
-							variant="h5"
-						>
-							First Name:
-						</Typography>
-						<TextField
-							style={{ width: 750, backgroundColor: "white" }}
-							variant="outlined"
-						></TextField>
-					</div>
-					<div className="detail">
-						<Typography
-							style={{ marginRight: 80 }}
-							className="label"
-							variant="h5"
-						>
-							Last Name:
-						</Typography>
-						<TextField
-							style={{ width: 750, backgroundColor: "white" }}
-							variant="outlined"
-						></TextField>
-					</div>
-					<div className="detail">
-						<Typography
-							style={{ marginRight: 140 }}
-							className="label"
-							variant="h5"
-						>
-							Email:
-						</Typography>
-						<TextField
-							style={{ width: 750, backgroundColor: "white" }}
-							variant="outlined"
-						></TextField>
-					</div>
-					<div className="detail">
-						<Typography
-							style={{ marginRight: 90 }}
-							className="label"
-							variant="h5"
-						>
-							Password:
-						</Typography>
-						<TextField
-							style={{ width: 750, backgroundColor: "white" }}
-							variant="outlined"
-							type="password"
-						></TextField>
-					</div>
-					<div className="submit">
-						<Button
-							variant="contained"
-							style={{ fontSize: 15 }}
-							color="primary"
-							endIcon={<ArrowRightAltIcon />}
-							size="large"
-						>
-							UPDATE DETAILS
-						</Button>
-					</div>
-				</Box>
-			</div>
-		);
-	}
+			if (!res.status === 200) {
+				throw "invalid attempt";
+			}
+		} catch (err) {
+			console.log(err);
+			history.push("/login");
+		}
+	};
+	useEffect(() => {
+		callProfilePage();
+	}, []);
+	return (
+		<div>
+			<Box className="form">
+				<div className="title">
+					<AccountCircleIcon
+						style={{ fontSize: 55, marginRight: 10 }}
+						className="icon"
+					/>
+					<Typography style={{ fontSize: 40 }} className="text" variant="h2">
+						{" "}
+						Welcome Back, {userInfo.username}!
+					</Typography>
+				</div>
+
+				<div className="detail">
+					<Typography
+						style={{ marginRight: 80 }}
+						className="label"
+						variant="h5"
+					>
+						First Name:
+					</Typography>
+					<TextField
+						value={userInfo.firstname}
+						style={{ width: 750, backgroundColor: "white" }}
+						variant="outlined"
+					></TextField>
+				</div>
+				<div className="detail">
+					<Typography
+						style={{ marginRight: 80 }}
+						className="label"
+						variant="h5"
+					>
+						Last Name:
+					</Typography>
+					<TextField
+						value={userInfo.lastname}
+						style={{ width: 750, backgroundColor: "white" }}
+						variant="outlined"
+					></TextField>
+				</div>
+				<div className="detail">
+					<Typography
+						style={{ marginRight: 140 }}
+						className="label"
+						variant="h5"
+					>
+						Email:
+					</Typography>
+					<TextField
+						value={userInfo.email}
+						style={{ width: 750, backgroundColor: "white" }}
+						variant="outlined"
+					></TextField>
+				</div>
+				<div className="detail">
+					<Typography
+						style={{ marginRight: 90 }}
+						className="label"
+						variant="h5"
+					>
+						Password:
+					</Typography>
+					<TextField
+						placeholder="Type a new password and submit this form to change"
+						style={{ width: 750, backgroundColor: "white" }}
+						variant="outlined"
+					></TextField>
+				</div>
+				<div className="submit">
+					<Button
+						variant="contained"
+						style={{ fontSize: 15 }}
+						color="primary"
+						endIcon={<ArrowRightAltIcon />}
+						size="large"
+					>
+						UPDATE DETAILS
+					</Button>
+				</div>
+			</Box>
+		</div>
+	);
 }
