5c700e1d6bc68e660808b1f99ba0caf709f846cd
ADIVADER19 <advaitnurani@gmail.com>
2022-09-04 08:05:33 +0530

    deployment 2

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,13 @@ module.exports = function override(config, env) {
     //do stuff with the webpack config...
 
     config.resolve.fallback = {
-        crypto: require.resolve('crypto-browserify'),
+        "crypto": require.resolve('crypto-browserify'),
+        // "stream": require.resolve("stream-browserify"), 
+        // "assert": require.resolve("assert"), 
+        // "http": require.resolve("stream-http"), 
+        // "https": require.resolve("https-browserify"), 
+        "os": require.resolve("os-browserify"), 
+        // "url": require.resolve("url") 
     };
     // config.plugins.push(
     //     new webpack.ProvidePlugin({
