af969c1db820032be3f0edf22c2d9da5360785b0
ADIVADER19 <advaitnurani@gmail.com>
2022-09-04 08:10:51 +0530

    deployment 2

--- config-overrides.js
+++ config-overrides.js
@@ -5,7 +5,7 @@ module.exports = function override(config, env) {
 
     config.resolve.fallback = {
         "crypto": require.resolve('crypto-browserify'),
-        // "stream": require.resolve("stream-browserify"), 
+        "stream": require.resolve("stream-browserify"), 
         // "assert": require.resolve("assert"), 
         // "http": require.resolve("stream-http"), 
         // "https": require.resolve("https-browserify"), 
